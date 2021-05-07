57c8f10dc7f8a21fe4f7184a89efacc58a91b5ad
Chlafen <bouchnak.amine03@gmail.com>
2021-05-08 00:06:25 +0100

    footer added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,52 @@
 import React from "react";
 
 export default function Footer(){
-    return(<footer>Aeroday Footer</footer>);
+    return(<div>
+        <div id="footer">
+        <div id="description">
+            <div id="photo"><img src="LogoWhite - Copy.png" alt="AERODAY"/></div>
+            <div id="desc">Tunisian Aeroday est l'unique journée nationale consacrée entièrement à l'Aéronautique en Tunisie, elle est fondée par le club Aerobotix insat en collaboration avec l'association des techniques de robotique (ATR). Cette journée est basée sur plusieurs axes et sur des différents challenges et elle opte pour la vulgarisation du domaine aéronautique chez le grand public ainsi que sur la découverte de nouveaux potentiels dont disposent les jeunes.</div>
+        </div>
+        <div id="keepintouch">
+            <div id="kit">KEEP IN TOUCH</div>
+            <div id="contact">
+            <div class="social">
+                    <div class="logo">
+                        <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M18.5605 10.1435L28 16.111V3.92395L18.5605 10.1435Z" fill="#519DA6"/>
+                            <path d="M0 3.92395V16.111L9.4395 10.1435L0 3.92395Z" fill="#519DA6"/>
+                            <path d="M26.25 0.375H1.74999C0.87674 0.375 0.18374 1.026 0.0524902 1.86425L14 11.0535L27.9475 1.86425C27.8162 1.026 27.1232 0.375 26.25 0.375Z" fill="#519DA6"/>
+                            <path d="M16.9575 11.2005L14.4813 12.8315C14.3343 12.9277 14.168 12.975 14 12.975C13.832 12.975 13.6658 12.9277 13.5188 12.8315L11.0425 11.1987L0.0560303 18.148C0.19078 18.9792 0.88028 19.625 1.75003 19.625H26.25C27.1198 19.625 27.8093 18.9792 27.944 18.148L16.9575 11.2005Z" fill="#519DA6"/>
+                        </svg>
+                            
+                    </div>
+                    <div class="info">temp_mail@insat.tn</div>
+                </div>
+                
+                <div class="social">
+                    <div class="logo">
+                        <svg width="20" height="30" viewBox="0 0 20 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.99999 0C4.48608 0 -3.05176e-05 4.48611 -3.05176e-05 10C-3.05176e-05 11.6553 0.413817 13.2965 1.20056 14.7522L9.45313 29.6777C9.563 29.8767 9.77235 30 9.99999 30C10.2276 30 10.437 29.8767 10.5468 29.6777L18.8025 14.7473C19.5862 13.2965 20 11.6552 20 9.99996C20 4.48611 15.5139 0 9.99999 0ZM9.99999 15C7.24304 15 5.00001 12.757 5.00001 10C5.00001 7.24307 7.24304 5.00004 9.99999 5.00004C12.7569 5.00004 15 7.24307 15 10C15 12.757 12.7569 15 9.99999 15Z" fill="#519DA6"/>
+                        </svg>                        
+                    </div>
+                    <div class="info">INSAT Centre Urbain Nord BP 676 - 1080 Tunis Cedex</div>
+                </div>
+                <div class="social">
+                    <div class="logo">
+                        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25.265 19.0814L21.6367 15.453C20.3408 14.1572 18.1379 14.6755 17.6195 16.3601C17.2308 17.5264 15.9349 18.1743 14.7687 17.9151C12.177 17.2672 8.67818 13.898 8.03026 11.1767C7.6415 10.0104 8.41901 8.71453 9.58527 8.32583C11.2699 7.80749 11.7882 5.60455 10.4924 4.3087L6.86399 0.680321C5.82731 -0.226774 4.27229 -0.226774 3.3652 0.680321L0.903086 3.14243C-1.55903 5.73413 1.16226 12.6021 7.25275 18.6926C13.3432 24.7831 20.2112 27.634 22.8029 25.0423L25.265 22.5802C26.1722 21.5435 26.1722 19.9885 25.265 19.0814Z" fill="#519DA6"/>
+                        </svg>
+                    </div>
+                    <p class="info">+216 23 815 004 &nbsp;&nbsp;&nbsp;&nbsp;+216 26 916 372</p>
+                    <p class="info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+216 28 455 462</p>
+                </div>
+                {/* <a href="https://www.facebook.com/"><div id="fb">fb</div></a>
+                <a href="https://www.instagram.com/"><div id="ig">ig</div></a> --> */}
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <p>Copyright Aeroday ©2021 All rights reserved</p>
+    </div>
+    </div>)
 }
