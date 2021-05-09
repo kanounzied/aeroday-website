fdda91d1a2b50e57a71e50c900d1d39049de75a9
Adolin <benmeft0@gamil.com>
2021-05-10 00:34:46 +0100

    add contact page without styling and plane animation

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 import {Component} from "react/cjs/react.production.min";
 import $ from 'jquery'
+import './contact.scss'
+import './form.scss'
 
 class Contact extends Component {
 
-    componentDidMount() {
-        $('#contact').addClass('selected');
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      plane_url: `${process.env.PUBLIC_URL}/assets/images/black-plane-icon1.png`,
+    };
+  }
 
-    render() {
-        return (
-            <div>Aeroday contact page</div>
-        );
-    }
+  componentDidMount() {
+    $('#contact').addClass('selected');
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div class="container">
+          <div className="right">
+            <input
+              type="text"
+              className="name"
+              placeholder="Nom & Prenom"
+            />
+            <input
+              type="text"
+              className="email"
+              placeholder="Email"
+            />
+            <button className="send-btn" >
+              ENVOYER
+            </button>
+          </div>
+          <div className="left">
+            <textarea
+              className="msg"
+              placeholder="Votre message"
+            />
+          </div>
+          <img src={this.state.plane_url} alt="plane" />
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Contact
