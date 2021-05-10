4cb18597f3912b90abd90e7e4db99f573a057cce
kanounzied <64085047+kanounzied@users.noreply.github.com>
2021-05-10 02:19:43 +0100

    fixed nav deleted header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,65 +1,55 @@
-import React from "react";
-import {Component} from "react/cjs/react.production.min";
+import React, {useEffect, useRef} from "react";
 import "../../App.css";
 import $ from 'jquery';
-import { ReactComponent as Logo } from './Logo.svg';
+import {ReactComponent as Logo} from './Logo.svg';
 
-class Navbar extends Component {
+export default function Navbar() {
+    const navRef = useRef();
 
-    componentDidMount() {
-        var navItem = $(".nav-items")
-        console.log(navItem.width())
+    useEffect(() => {
         $('.logo-svg g').attr('fill','#ffffff')
-
         $(window).scroll(function (){
-                if ($(window).scrollTop() >= 200) {
-                    $('.navbar').addClass('smaller')
-                    $('.logo-svg g').attr('fill','#000000')
-                    //logo.attr('src', logo.attr('src').replace('White', 'Black'))
-                } else {
-                    //logo.attr('src', logo.attr('src').replace('Black', 'White'))
-                    $('.navbar').removeClass('smaller')
-                    $('.logo-svg g').attr('fill','#ffffff')
-                }
+            if ($(window).scrollTop() >= 200) {
+                $('.navbar').addClass('smaller')
+                $('.logo-svg g').attr('fill','#000000')
+                //logo.attr('src', logo.attr('src').replace('White', 'Black'))
+            } else {
+                //logo.attr('src', logo.attr('src').replace('Black', 'White'))
+                $('.navbar').removeClass('smaller')
+                $('.logo-svg g').attr('fill','#ffffff')
+            }
         })
-    }
+    })
 
-    render() {
-        const brandStyle = {
-            height: "50px",
-        }
-
-        return (
-            <>
-                <div className="navbar">
-                    <a href="/" className={"logo"}><Logo className="logo-svg" height="auto" width="190px" color={"#ffffff"}/>
-                        {/*<img src={process.env.PUBLIC_URL + "/assests/images/LogoWhite.png"}*/}
-                        {/*                                alt="aeroday logo" style={brandStyle}/>*/}
-                    </a>
-                    <div className="nav-items">
-                        <ul>
-                            <li className={"nav-item"} id={"home"}><a href="/">Acceuil</a></li>
-                            <li className={"nav-item"} id={"axes"}><a href="/#axes&challenges">Axes & Challenges</a></li>
-                            <li className={"nav-item"} id={"about"}><a href="/about">A Propos De Nous</a></li>
-                            <li className={"nav-item"} id={"contact"}><a href="/contact">Contact Us</a></li>
-                        </ul>
-                        <div className="dropdown">
-                            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                ☰
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" href="/">Acceuil</a>
-                                <a className="dropdown-item" href="/#axes&challenges">Axes & Challenges</a>
-                                <a className="dropdown-item" href="/about">A Propos De Nous</a>
-                                <a className="dropdown-item" href="/contact">Contact</a>
-                            </div>
+    return (
+        <>
+            <div className="navbar" ref={navRef}>
+                <a href="/" className={"logo"}><Logo className="logo-svg" height="auto" width="190px"
+                                                     color={"#ffffff"}/>
+                    {/*<img src={process.env.PUBLIC_URL + "/assests/images/LogoWhite.png"}*/}
+                    {/*                                alt="aeroday logo" style={brandStyle}/>*/}
+                </a>
+                <div className="nav-items">
+                    <ul>
+                        <li className={"nav-item"} id={"home"}><a href="/">Acceuil</a></li>
+                        <li className={"nav-item"} id={"axes"}><a href="/#axes&challenges">Axes & Challenges</a></li>
+                        <li className={"nav-item"} id={"about"}><a href="/about">A Propos De Nous</a></li>
+                        <li className={"nav-item"} id={"contact"}><a href="/contact">Contact Us</a></li>
+                    </ul>
+                    <div className="dropdown">
+                        <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            ☰
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" href="/">Acceuil</a>
+                            <a className="dropdown-item" href="/#axes&challenges">Axes & Challenges</a>
+                            <a className="dropdown-item" href="/about">A Propos De Nous</a>
+                            <a className="dropdown-item" href="/contact">Contact</a>
                         </div>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 }
-
-export default Navbar
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./home.css";
-import {Component} from "react/cjs/react.production.min";
 import $ from 'jquery'
 
-class Home extends Component {
+export default function Home() {
 
-    componentDidMount() {
+    useEffect(() => {
         $('#home').addClass('selected');
-    }
+    });
 
-    render() {
-        return (
-            <div className="home">Aeroday Home page</div>
-        );
-    }
+    return (
+        <div className="home">Aeroday Home page</div>
+    );
 }
-
-export default Home
