986b5973afb0d2fd3b603d9f1990a748adf587cf
Chlafen <bouchnak.amine03@gmail.com>
2021-05-11 22:33:41 +0100

    about us template

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {Component} from "react/cjs/react.production.min";
 import $ from 'jquery'
+import MemberCard from "../../components/Member"
+import './about.scss'
+
 
 class About extends Component {
 
@@ -10,7 +13,22 @@ class About extends Component {
 
     render() {
         return (
-            <div>Aeroday About page</div>
+            <div id="about-us-body">
+                <div id="about-us-title">
+                    <h1>A PROPOS DE NOUS</h1>
+                </div>
+                <div id="about-us-video-holder">
+                    <div id="about-us-video">
+                        <h2>C'EST QUOI L'AERODAY?</h2>
+                        <video autoplay="" loop muted="" width="100%" height="100%" preload="auto" autobuffer="true" muted>
+                            <source type="video/mp4" src={ process.env.PUBLIC_URL + "/assests/videos/aboutusvideo.mp4" }/>
+                        </video>
+                    </div>
+                </div>
+                <div id="team-holder">
+                    <MemberCard memberName = "Khouloud Hamrouni" memberOccupation="PRESIDENT"/>
+                </div>
+            </div>
         );
     }
 }
