422ccdfc9ed7798da3d0a1bc1b110eee778716b7
Chlafen <bouchnak.amine03@gmail.com>
2021-05-12 22:45:21 +0100

    ac body

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import {Component} from "react/cjs/react.production.min";
 import $ from 'jquery'
 import Intro from "../../components/Intro"
 import ACbody from "../../components/AC body"
-
+import Sidebar from "../../components/AC sidebar"
 
 const desc_aeroday=<p>Revient pour la cinquième année consécutive dans <span>Le Tunisian Aeroday</span>. Les participants devront <span>construire</span> leurs 
 propres <span>modèles</span> de planeur de taille réduite,les <span>présenter</span> à un public de professionnels et d'amateurs et les faire <span>voler</span>. 
@@ -17,9 +17,12 @@ export default function Home() {
     });
 
         return (
-            <div className="home">
+            <div className="home" style={{height:"2000px"}} >
                 <Intro />
-                <ACbody description={desc_aeroday}/>
+                <ACbody titleAC="CHALLENGES" acType="Aeromodelisme" description={desc_aeroday}   
+                        memberName="Haizem Dahech" memberOccupation="RESP. AEROMODELISME" 
+                        cdc1="1"   cdc2="0"   cdes="1"
+                        CDC1URL="https://www.facebook.com/" CDC2URL="https://www.facebook.com/" CDESURL="https://www.facebook.com/" />  
             </div>
         );
 }
